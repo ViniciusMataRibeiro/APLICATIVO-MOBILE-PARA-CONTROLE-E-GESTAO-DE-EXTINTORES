0bfdcda749dc45facaf6cc71e68bf7a53a14d53d
Marcos Fagundes <75048100+Fagundes97@users.noreply.github.com>
2022-12-20 09:02:27 -0300

    ;

--- CGE_Api/database/migrations/1671536396186_users.ts
+++ CGE_Api/database/migrations/1671536396186_users.ts
@@ -9,12 +9,7 @@ export default class extends BaseSchema {
       table.string('email', 255).notNullable().unique()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
-
-      /**
-       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('created_at', { useTz: true }).notNullable()
-      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamps(true, true)
     })
   }
 
