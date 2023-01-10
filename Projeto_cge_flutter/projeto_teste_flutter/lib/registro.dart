5f8daa1a1613f1888529826a02de186d1e64a490
Marcus Paulo <71295372+marcuscmd@users.noreply.github.com>
2023-01-10 19:54:24 -0300

    Formulario2

--- Projeto_cge_flutter/projeto_teste_flutter/lib/registro.dart
+++ Projeto_cge_flutter/projeto_teste_flutter/lib/registro.dart
@@ -10,6 +10,42 @@ class Registro extends StatelessWidget {
         centerTitle: true,
         title: const Text('Registro de Empresa'),
         backgroundColor: const Color.fromARGB(255, 175, 31, 21),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/image/registro.jpeg'),
+              fit: BoxFit.cover)
+          ),
+          child: Column(
+          children: [
+            Center(
+              child: Container(
+                height: 140,
+                width: 140,
+                margin: const EdgeInsets.only(top: 25),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(image: AssetImage('assets/image/cge.jpeg'),
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 10,
+                      offset: Offset(10, 10),
+                      ),
+                  ]
+                ),
+              ),
+            ),
+          ]
+        ),      
+      ),
       ),
     );
   }
