8b9548aea48efed730423099668b5349d2797f05
ViniciusMataRibeiro <74631335+ViniciusMataRibeiro@users.noreply.github.com>
2023-01-17 21:40:01 -0300

    ajuste rota tecnico

--- CGE_Api/start/routes.ts
+++ CGE_Api/start/routes.ts
@@ -4,12 +4,13 @@ Route.post('/login', 'AuthController.login');
 Route.post('/logout', 'AuthController.logout');
 
 Route.post('/empresa/cadastro', 'EmpresasController.store');
+Route.post('/tecnico/cadastro/:id', 'TecnicosController.store');
 
 Route.group(() => {
   Route.get('auth/me', 'AuthController.me');
 
   Route.get('/Tecnicos', 'TecnicosController.index');
-  Route.post('/tecnico/cadastro/:id', 'TecnicosController.store');
+
   Route.put('/tecnico', 'TecnicosController.update');
   Route.delete('/tecnico/:id', 'TecnicosController.destroy');
 
