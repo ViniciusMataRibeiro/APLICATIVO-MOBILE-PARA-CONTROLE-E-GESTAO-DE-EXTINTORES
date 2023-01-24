ade95a72eaaab68c7eb04326a18388313eee0a84
Marcus Paulo <71295372+marcuscmd@users.noreply.github.com>
2023-01-23 21:53:13 -0300

    TentativaFalha_Navegacao

--- Flutter_CGE_testando/app_cge/lib/app/modules/login/page.dart
+++ Flutter_CGE_testando/app_cge/lib/app/modules/login/page.dart
@@ -1,9 +1,12 @@
+import 'dart:js';
+
 import 'package:app_cge/app/modules/login/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginPage extends GetView<LoginController> {
   const LoginPage({super.key});
+
 
   @override
   Widget build(BuildContext context) {
