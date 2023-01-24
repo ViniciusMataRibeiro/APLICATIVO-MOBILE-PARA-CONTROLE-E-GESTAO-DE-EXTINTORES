9e567acf97a8a9d51f73b29cb523a18cdaf1b4d4
ViniciusMataRibeiro <74631335+ViniciusMataRibeiro@users.noreply.github.com>
2023-01-24 19:55:44 -0300

    Revert "TentativaFalha_Navegacao"
    This reverts commit ade95a72eaaab68c7eb04326a18388313eee0a84.

--- Flutter_CGE_testando/app_cge/lib/app/modules/login/controller.dart
+++ Flutter_CGE_testando/app_cge/lib/app/modules/login/controller.dart
@@ -1,10 +1,7 @@
-import 'dart:js';
-
 import 'package:app_cge/app/data/models/user_login_request.dart';
 import 'package:app_cge/app/data/services/auth/service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 
 class LoginController extends GetxController {
   final _authService = Get.find<AuthService>();
@@ -12,8 +9,6 @@ class LoginController extends GetxController {
   var showPassword = true.obs;
   var emailController = TextEditingController(text: 'admin@email.com');
   var passwordController = TextEditingController(text: '123456');
-  
-
 
   void toggleShowPassword() {
     showPassword.value = !showPassword.value;
@@ -22,7 +17,7 @@ class LoginController extends GetxController {
   void goTologin() {
     var userLoginRequestModel = UserLoginRequestModel(
         email: emailController.text, password: passwordController.text);
-      
+
     _authService.login(userLoginRequestModel).then((value) => null);
   }
 }
