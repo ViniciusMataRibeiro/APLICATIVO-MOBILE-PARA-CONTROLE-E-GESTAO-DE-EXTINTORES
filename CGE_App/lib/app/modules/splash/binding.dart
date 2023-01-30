ffea15ebcda1bab9c053d7cd10bf8046d8e1980f
Fagundes97 <75048100+Fagundes97@users.noreply.github.com>
2023-01-30 19:51:28 -0300

    Alguns Ajustes

--- CGE_App/lib/app/modules/splash/binding.dart
+++ CGE_App/lib/app/modules/splash/binding.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import '../login/controller.dart';
@@ -8,5 +9,6 @@ class SplashBinding implements Bindings {
   void dependencies() {
     Get.lazyPut<SplashController>(() => SplashController());
     Get.lazyPut<LoginController>(() => LoginController());
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   }
 }
