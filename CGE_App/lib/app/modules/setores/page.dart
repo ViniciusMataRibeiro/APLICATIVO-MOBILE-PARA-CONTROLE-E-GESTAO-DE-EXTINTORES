0ec219206fa844894532283c839b7666da38e1f9
Vinicius <vinimataribeiro@hotmail.com.br>
2023-01-30 21:57:12 -0300

    teste do botao flutuante

--- CGE_App/lib/app/modules/setores/page.dart
+++ CGE_App/lib/app/modules/setores/page.dart
@@ -138,3 +138,85 @@ class SetorPage extends GetView<SetorController> {
         )));
   }
 }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage>
+//     with SingleTickerProviderStateMixin {
+//   Animation<double>? _animation;
+//   AnimationController? _animationController;
+
+//   @override
+//   void initState() {
+//     _animationController = AnimationController(
+//       vsync: this,
+//       duration: Duration(milliseconds: 260),
+//     );
+
+//     final curvedAnimation =
+//         CurvedAnimation(curve: Curves.easeInOut, parent: _animationController!);
+//     _animation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
+
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           centerTitle: true,
+//           title: const Text("Animated FAB Menu Option"),
+//         ),
+//         body: Container(
+//           height: double.infinity,
+//           color: Colors.yellowAccent[100],
+//         ),
+//         floatingActionButton: FloatingActionBubble(
+//           items: <Bubble>[
+//             Bubble(
+//               title: "Settings",
+//               iconColor: Colors.white,
+//               bubbleColor: Colors.blue,
+//               icon: Icons.settings,
+//               titleStyle: TextStyle(fontSize: 16, color: Colors.white),
+//               onPress: () {
+//                 _animationController!.reverse();
+//               },
+//             ),
+//             Bubble(
+//               title: "Profile",
+//               iconColor: Colors.white,
+//               bubbleColor: Colors.blue,
+//               icon: Icons.people,
+//               titleStyle: TextStyle(fontSize: 16, color: Colors.white),
+//               onPress: () {
+//                 _animationController!.reverse();
+//               },
+//             ),
+//             Bubble(
+//               title: "Home",
+//               iconColor: Colors.white,
+//               bubbleColor: Colors.blue,
+//               icon: Icons.home,
+//               titleStyle: TextStyle(fontSize: 16, color: Colors.white),
+//               onPress: () {
+//                 _animationController!.reverse();
+//               },
+//             ),
+//           ],
+//           animation: _animation!,
+//           onPress: () => _animationController!.isCompleted
+//               ? _animationController!.reverse()
+//               : _animationController!.forward(),
+//           backGroundColor: Colors.blue,
+//           iconColor: Colors.white,
+//           iconData: Icons.menu,
+//         ));
+//   }
+// }
+
