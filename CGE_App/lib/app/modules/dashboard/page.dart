6511cf245482a952647db92695a1fc7d90d7ae7b
Vinicius <vinimataribeiro@hotmail.com.br>
2023-02-01 22:29:21 -0300

    menu de baixo

--- CGE_App/lib/app/modules/dashboard/page.dart
+++ CGE_App/lib/app/modules/dashboard/page.dart
@@ -1,53 +1,54 @@
 import 'package:cge_app/app/data/services/auth/service.dart';
+import 'package:cge_app/app/modules/extintores/page.dart';
+import 'package:cge_app/app/modules/setores/page.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../abas/page.dart';
 import '../perfil/page.dart';
 import '../perfil_tecnico/page.dart';
 import 'controller.dart';
 
 class DashboardPage extends GetView<DashboardController> {
   AuthService aux = Get.find<AuthService>();
+
   @override
   Widget build(BuildContext context) {
     final Items = <Widget>[
-      const Icon(
-        Icons.view_stream_rounded,
-        size: 40,
-        color: Colors.white,
-      ),
-      const Icon(
-        Icons.person,
-        size: 40,
-        color: Colors.white,
-      ),
+      const Icon(Icons.home, size: 30, color: Colors.white),
+      const Icon(Icons.shop, size: 30, color: Colors.white),
+      const Icon(Icons.messenger_sharp, size: 30, color: Colors.white),
+      const Icon(Icons.person, size: 30, color: Colors.white),
     ];
     var obj = aux.user.value;
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
         bottomNavigationBar: Obx(
           () => CurvedNavigationBar(
             onTap: controller.changePage,
-            animationCurve: Curves.easeInOut,
             height: 60,
-            index: controller.currentPage.value,
-            color: const Color.fromARGB(255, 190, 0, 0),
-            animationDuration: const Duration(milliseconds: 300),
             backgroundColor: Colors.transparent,
+            color: const Color.fromARGB(255, 190, 0, 0),
+            index: controller.currentPage.value,
             items: Items,
           ),
         ),
-        body: Obx(() => IndexedStack(
-              index: controller.currentPage.value,
-              children: [
-                AbaPage(),
-                if (obj!.tipo == 'empresas') ...[
-                  PerfilPage(),
-                ] else ...[
-                  PerfilTecnicoPage(),
-                ]
-              ],
-            )));
+        extendBody: true,
+        body: Obx(
+          () => IndexedStack(
+            index: controller.currentPage.value,
+            children: [
+              Setor(),
+              Extintor(),
+              if (obj!.tipo == 'empresas') ...[
+                PerfilPage(),
+              ] else ...[
+                PerfilTecnicoPage(),
+              ]
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
